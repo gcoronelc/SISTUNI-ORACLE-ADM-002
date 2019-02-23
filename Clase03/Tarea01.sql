54e06605cfd7f25821f259ac0a188fc3f2341616
Gustavo Coronel <gcoronelc@gmail.com>
2019-02-23 12:13:53 -0500

    Clase 03

---
+++ Clase03/Tarea01.sql
@@ -0,0 +1,9 @@
+/*
+1.- Crear el esquema EUREKA
+2.- Crear el procedimiento registrar deposito
+3.- Crear el usuario EUREKA_QUERY, solo debe 
+    poder consultar todas las tablas de EUREKA
+4.- Crear el usuario EUREKA_EXECUTE solo debe
+    poder ejecutar procedimientos almacenados de EUREKA
+5.- Hacer una prueba de cada usuario.
+*/
